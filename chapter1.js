53f242ab069a976e1189e1f576331e2a3eaaba1f
Daparrag <daparrag@correo.udistrital.edu.co>
2017-12-06 10:55:37 +0100

    update chapter 1

--- chapter1.js
+++ chapter1.js
@@ -24,21 +24,56 @@ OK, go ahead. Try it. Don't peek at my code listing until you've given it a shot
 */
 
 var bank_balance;
-var purchase;
+var purchase=0;
+var threshold;
 
 const TAX_RATE = 0.08;
-const ACCESSORY_PRICE = 9.9;
+const ACCESSORY_PRICE = 10;
 const PHONE_PRICE = 100;
 
 
 
  function main()
  {
- 	while(true){
- 		var op =  prompt( "typing (1) if you whan buy a phone" );
+ 	    do{
+ 	    	var a=1;
+ 	    	var bl = prompt( "insert your bank balance " );
+ 			bank_balance = Number(bl);
+ 			var th = prompt( "insert your purchase threshold " );
+ 			threshold = Number(th);
+ 			if(threshold >= bank_balance)
+ 			{
+ 				a=0;
+ 				console.log("threshold must be less than equal than bank balance");
+ 			}
+ 	    }while(a==0);
+ 		
+ 		
 
+ 	while(true)
+ 	{		 	
+ 		var op =  prompt( "type: (1) if you want to buy a phone. (2) if you want buy an accessory. (3) pay " );
+ 		
  		if(op==1){
+ 			if(bank_balance >= PHONE_PRICE && purchase <= threshold){
+ 				bank_balance -=PHONE_PRICE;
+ 				purchase += PHONE_PRICE;		
+ 			}else{
+ 				console.log("Your balance is insuficient for complete this operation");
+
+ 			}
  			
+ 		}
+
+ 		if(op==2){
+ 			if(bank_balance >= ACCESSORY_PRICE && purchase <= threshold )
+ 			{
+ 				bank_balance -=ACCESSORY_PRICE;
+ 				purchase += ACCESSORY_PRICE;
+
+ 			}else{
+ 				console.log("Your balance is insuficient for complete this operation");
+ 			}
  		}
  	}
  }
